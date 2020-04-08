89d475bfc3c529eca532ae7d3ba3404f2543ed4a
yixianghsy <yixianghsy@gmail.com>
2020-04-08 23:50:49 +0800

    git@github.com:yixianghsy/myblog.git

---
+++ .history/db/config_20200408221859.js
@@ -0,0 +1,21 @@
+let dbOption
+
+// dbOption = {
+//   connectionLimit: 10,
+//   host: 'localhost',
+//   user: 'root',
+//   password: '123456789',
+//   port: '3306',
+//   database: 'xd_blog'
+// }
+
+dbOption = {
+  connectionLimit: 10,
+  host: '120.24.80.45',
+  user: 'root',
+  password: 'root',
+  port: '3306',
+  database: 'xd_blog'
+}
+
+module.exports = dbOption
